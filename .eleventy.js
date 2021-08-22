575f42b293ee7ad9fcf120162077a2b5f8df1e75
Louise <louise.gilligan@zoopla.co.uk>
2021-08-22 13:51:29 +0100

    edit

--- .eleventy.js
+++ .eleventy.js
@@ -103,30 +103,14 @@ module.exports = function(eleventyConfig) {
       "liquid"
     ],
 
-    // -----------------------------------------------------------------
-    // If your site deploys to a subdirectory, change `pathPrefix`.
-    // Don’t worry about leading and trailing slashes, we normalize these.
-
-    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
-    // This is only used for link URLs (it does not affect your file structure)
-    // Best paired with the `url` filter: https://www.11ty.dev/docs/filters/url/
-
-    // You can also pass this in on the command line using `--pathprefix`
-
-    // Optional (default is shown)
     pathPrefix: "/",
-    // -----------------------------------------------------------------
 
-    // Pre-process *.md files with: (default: `liquid`)
     markdownTemplateEngine: "njk",
 
-    // Pre-process *.html files with: (default: `liquid`)
     htmlTemplateEngine: "njk",
 
-    // Opt-out of pre-processing global data JSON files: (default: `liquid`)
     dataTemplateEngine: false,
 
-    // These are all optional (defaults are shown):
     dir: {
       input: ".",
       includes: "_includes",
